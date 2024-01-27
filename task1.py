4f937d88366d57401ea9548795cd55bb2909248e
Anastasia-front <artemalex1990@gmail.com>
2024-01-27 16:46:00 +0200

    Update task1.py
    Co-Authored-By: Anastasiia Pzysiazhna <115567693+anastasia-front@users.noreply.github.com>

--- task1.py
+++ task1.py
@@ -1,11 +1,15 @@
+import re
 from datetime import datetime
 
-def get_days_from_today(date = None):
+
+def get_days_from_today(date=None):
     current_datetime = datetime.today()
 
     while True:
         if date is None:
-            date = input("Write a date in format YYYY.MM.DD (for instance - 2012.12.20): ")
+            date = input(
+                "Write a date in format YYYY.MM.DD (for instance - 2012.12.20): "
+            )
 
         try:
             datetime_object = datetime.strptime(date, "%Y.%m.%d")
@@ -18,53 +22,75 @@ def get_days_from_today(date = None):
                     """Result : 
                     There is no significant difference; 
                     you either wrote today's date or 
-                    the date is within one day of today."""
+                    the date is within one day of today.\n"""
                 )
+                break
             else:
-                print(f"Result : The difference is {difference.days} days.")
-            break
+                print(f"\nResult : The difference is {difference.days} days. \n")
+                break
         except ValueError as error:
-            print("1. You wrote date in wrong format")
+            print("\n1. You wrote date in wrong format")
             print(f"2. Error: {error}")
-            print("3. Please enter a valid date")
+            print("3. Please enter a valid date\n")
+            date = None
         except Exception as error:
-            print(f"Unexpected error: {error}")
+            print(f"\nUnexpected error: {error}")
+            date = None
+
         finally:
-            print("- Loop is over")
+            print("- Loop is over \n")
 
+
+print("-" * 30)
+print("this will prompt the user for input")
+print("-" * 30, "\n")
 get_days_from_today()  # this will prompt the user for input
+print("-" * 30)
+print(" a date string passed as an argument")
+print("-" * 30, "\n")
 get_days_from_today("2023.12.14")  # pass a date string as an argument
-
+print("~" * 30)
 # example with usage of regexp
-
-# import re
-# from datetime import datetime
-
-# current_datetime = datetime.today()
-# pattern = "^\d{4}\.(?:0[1-9]|1[0-2])\.(?:0[1-9]|[12][0-9]|3[01])$"
+print("with usage of regexp")
+print("~" * 30, "\n\n")
 
 
-# def get_days_from_today(date):
-#     check = re.search(pattern, date)
+def get_days_from_today(date=None):
+    current_datetime = datetime.today()
+    pattern = "^\d{4}\.(?:0[1-9]|1[0-2])\.(?:0[1-9]|[12][0-9]|3[01])$"
 
-#     if check:
-#         datetime_object = datetime.strptime(date, "%Y.%m.%d")
+    if date is None:
+        date = input("Write a date in format YYYY.MM.DD (for instance - 2012.12.20): ")
+        check = re.search(pattern, date)
 
-#         current_date = current_datetime.date()
-#         target_date = datetime_object.date()
+    check = re.search(pattern, date)
 
-#         difference = current_date - target_date
+    if check:
+        datetime_object = datetime.strptime(date, "%Y.%m.%d")
 
-#         if difference.days > 1 or difference.days < -1:
-#             print(f"The difference is {difference.days} days")
-#         elif difference.days != 0:
-#             print(f"The difference is {difference.days} day")
-#         else:
-#             print("There is no difference, you wrote today date")
+        current_date = current_datetime.date()
+        target_date = datetime_object.date()
 
-#     else:
-#         print("You wrote date in wrong format")
+        difference = current_date - target_date
+
+        if difference.days > 1 or difference.days < -1:
+            print(f"The difference is {difference.days} days\n")
+        elif difference.days != 0:
+            print(f"The difference is {difference.days} day\n")
+        else:
+            print("There is no difference, you wrote today date\n")
+
+    else:
+        print("\nYou wrote date in wrong format\n")
+        date = None
 
 
-# date = input("Write a date in format YYYY.MM.DD (for instance - 2012.12.20) : ")
-# get_days_from_today(date)
+print("-" * 30)
+print("this will prompt the user for input")
+print("-" * 30, "\n")
+get_days_from_today()  # this will prompt the user for input
+print("-" * 30)
+print(" a date string passed as an argument")
+print("-" * 30, "\n")
+get_days_from_today("2022.02.24")  # pass a date string as an argument
+print("-" * 30)
